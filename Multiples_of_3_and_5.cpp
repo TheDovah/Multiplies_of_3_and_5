d5dc2d39ff6b2a26488583e13ae05b4479ad3e38
TheDovah <johasja@outlook.com>
2019-09-26 16:34:59 +0100

    Needs some optimising

--- Multiples_of_3_and_5.cpp
+++ Multiples_of_3_and_5.cpp
@@ -6,38 +6,103 @@ int main ()
     int Number3 = 3;
     int Number5 = 5;
 
+    int OldNumber3;
+    int OldNumber5;
+
+    int NewNumber3;
+    int NewNumber5;
+
     int i = 1;
     
+    
+
     std::cout << "Original value of Number3: " << Number3 << std::endl;
     std::cout << "Original value of Number5: " << Number5 << std::endl;
     std::cout << "Original value of i: " << i << std::endl;
     std::cout << "-----------------------" << std::endl;
 
-    while (i < 4)
-    {
-        int NewNumber3 = i*Number3;
-        int NewNumber5 = i*Number5;
+    
+    do
+    {   
 
-        std::cout << "value of NewNumber3: " << NewNumber3 << std::endl;
-        std::cout << "value of NewNumber5: " << NewNumber5 << std::endl;
+        
+        int NewNumber3 = i * Number3;
+        int NewNumber5 = i * Number5;
+                
 
-        int AddedNumber = NewNumber3 + NewNumber5;
+        std::cout << "Value of OldNumber3 before the if: " << OldNumber3 << std::endl;
+        std::cout << "Value of OldNumber5 before the if: " << OldNumber5 << std::endl;
+        
+       
 
-        if (NewNumber3 < 10 || NewNumber5 < 10)
+        if (OldNumber3 < 10 > NewNumber3 && OldNumber5 < 10 > NewNumber5)
         {
         
-        std::cout << "value of the numbers added is: " << AddedNumber << std::endl;
-        std::cout << "" << std::endl;
-        
+            int NewNumber5 = OldNumber5;
+            int NewNumber3 = OldNumber3;
+            int AddedNumber = NewNumber3 + NewNumber5;
 
+        std::cout << "" << std::endl;
+        std::cout << "asdfdasdfsfasdfsfadfsfds" << std::endl;
+        std::cout << "Value of NewNumber3: " << NewNumber3 << std::endl;
+        std::cout << "Value of NewNumber5: " << NewNumber5 << std::endl;
+        std::cout << "Value of i: " << i << std::endl;
+        std::cout << "-----------------------" << std::endl;
+        std::cout << "Value of OldNumber3 " << OldNumber3 << std::endl;
+        std::cout << "Value of OldNumber5 " << OldNumber5 << std::endl;
+        std::cout << "" << std::endl;
+
+        std::cout << "-----------------------" << std::endl;
+
+        std::cout << "Value of the numbers added is: " << AddedNumber << std::endl;
+        std::cout << "-----------------------" << std::endl;
+        std::cout << "-----------------------" << std::endl;
+        std::cout << "" << std::endl;
+
+            return 0;
         }
         else
         {
-            i++;
-        }
+            int AddedNumber = NewNumber3 + NewNumber5;
 
-    }
+        std::cout << "-----------------------" << std::endl;
+        std::cout << "Value of NewNumber3: " << NewNumber3 << std::endl;
+        std::cout << "Value of NewNumber5: " << NewNumber5 << std::endl;
+        std::cout << "Value of i: " << i << std::endl;
+        std::cout << "-----------------------" << std::endl;
+        std::cout << "Value of OldNumber3 " << OldNumber3 << std::endl;
+        std::cout << "Value of OldNumber5 " << OldNumber5 << std::endl;
+        std::cout << "" << std::endl;
+
+        std::cout << "-----------------------" << std::endl;
+
+        std::cout << "Value of the numbers added is: " << AddedNumber << std::endl;
+        std::cout << "-----------------------" << std::endl;
+        std::cout << "-----------------------" << std::endl;
+        std::cout << "" << std::endl;
+        }
+        
+
+        
+        
+        
+        
+        
+
+
+
+        OldNumber3 = NewNumber3;
+        OldNumber5 = NewNumber5;
+        
+
+        i++;
     
+    }
+    while (i <= 5);
+
+
+
+
     return 0;
     
     

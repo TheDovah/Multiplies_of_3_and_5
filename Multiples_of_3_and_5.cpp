0314686c68953dc3a2ba7eae5473ce47ce32e844
TheDovah <johasja@outlook.com>
2019-09-26 13:55:00 +0100

    added the program itself, and actually made it multiply and add

--- Multiples_of_3_and_5.cpp
+++ Multiples_of_3_and_5.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
+#include <vector>
 
-int Numbercount ()
+int main ()
 {
     int Number3 = 3;
     int Number5 = 5;
 
-    int my_array3[]{};
-    int my_array5[]{}
+    int i = 1;
+    
+    std::cout << "Original value of Number3: " << Number3 << std::endl;
+    std::cout << "Original value of Number5: " << Number5 << std::endl;
+    std::cout << "Original value of i: " << i << std::endl;
+    std::cout << "-----------------------" << std::endl;
 
-    /*
-    while (i <= 1000)
+    while (i < 4)
     {
         int NewNumber3 = i*Number3;
         int NewNumber5 = i*Number5;
 
-        std::cout << NewNumber3 << std::endl;
-        std::cout << NewNumber5 << std::endl;
+        std::cout << "value of NewNumber3: " << NewNumber3 << std::endl;
+        std::cout << "value of NewNumber5: " << NewNumber5 << std::endl;
 
-        i++;
+        int AddedNumber = NewNumber3 + NewNumber5;
+
+        if (NewNumber3 < 10 || NewNumber5 < 10)
+        {
+        
+        std::cout << "value of the numbers added is: " << AddedNumber << std::endl;
+        std::cout << "" << std::endl;
+        
+
+        }
+        else
+        {
+            i++;
+        }
+
     }
-    */
-
-    return 1;
+    
+    return 0;
+    
     
 }

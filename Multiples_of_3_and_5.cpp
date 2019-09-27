f2e5cea2cac4615ad960ec62b51a3373d569517c
TheDovah <johasja@outlook.com>
2019-09-27 17:13:29 +0100

    Close...!

--- Multiples_of_3_and_5.cpp
+++ Multiples_of_3_and_5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int main ()
 {
@@ -12,118 +13,105 @@ int main ()
     int NewNumber3;
     int NewNumber5;
 
+    int GoodNumber3 = 0;
+    int GoodNumber5 = 0;
+
     int i = 1;
     
     
 
-    std::cout << "Original value of Number3: " << Number3 << std::endl;
-    std::cout << "Original value of Number5: " << Number5 << std::endl;
-    std::cout << "Original value of i: " << i << std::endl;
-    std::cout << "-----------------------" << std::endl;
+    cout << "Original value of Number3: " << Number3 << endl;
+    cout << "Original value of Number5: " << Number5 << endl;
+    cout << "Original value of i: " << i << endl;
+    cout << "-----------------------" << endl;
 
-/*
-    do
+
+    while (i <= 5)
     {   
-
-        
         int NewNumber3 = i * Number3;
         int NewNumber5 = i * Number5;
                 
+        cout << "Value of Number3: " << Number3 << endl;
+        cout << "Value of Number5: " << Number5 << endl;
+        cout << "Value of NewNumber3: " << NewNumber3 << endl;
+        cout << "Value of OldNumber3: " << OldNumber3 << endl;
+        cout << "Value of NewNumber5: " << NewNumber5 << endl;
+        cout << "Value of OldNumber5: " << OldNumber5 << endl;
+        cout << "Value of i: " << i << endl;
+        cout << "-----------------------" << endl;
 
-        std::cout << "Value of OldNumber3 before the if: " << OldNumber3 << std::endl;
-        std::cout << "Value of OldNumber5 before the if: " << OldNumber5 << std::endl;
-        
-       
-
-        if (5 < OldNumber3 < 10 > NewNumber3 && 5 < OldNumber5 < 10 > NewNumber5)
+        if (OldNumber3 > 10)
         {
-        
-            int NewNumber5 = OldNumber5;
-            int NewNumber3 = OldNumber3;
-            int AddedNumber = NewNumber3 + NewNumber5;
-
-        std::cout << "" << std::endl;
-        std::cout << "asdfdasdfsfasdfsfadfsfds" << std::endl;
-        std::cout << "Value of NewNumber3: " << NewNumber3 << std::endl;
-        std::cout << "Value of NewNumber5: " << NewNumber5 << std::endl;
-        std::cout << "Value of i: " << i << std::endl;
-        std::cout << "-----------------------" << std::endl;
-        std::cout << "Value of OldNumber3 " << OldNumber3 << std::endl;
-        std::cout << "Value of OldNumber5 " << OldNumber5 << std::endl;
-        std::cout << "" << std::endl;
-
-        std::cout << "-----------------------" << std::endl;
-
-        std::cout << "Value of the numbers added is: " << AddedNumber << std::endl;
-        std::cout << "-----------------------" << std::endl;
-        std::cout << "***********************" << std::endl;
-        std::cout << "" << std::endl;
-
+            cout << "WRONG" << endl;
             return 0;
         }
-        else
+
+        if (OldNumber3 <= 10)
         {
-            int AddedNumber = NewNumber3 + NewNumber5;
+            if (NewNumber3 > 10)
+            {
+                GoodNumber3 = NewNumber3;
+                /*NewNumber5 = OldNumber5;*/
+                cout << "" << endl;
+                cout << "NewNumber3 if statement" << endl;
+                cout << "Goodnumber3: " << GoodNumber3 << endl;
+                cout << "Goodnumber5: " << GoodNumber5 << endl;
+                cout << "Newnumber3: " << NewNumber3 << endl;
+                cout << "NewNumber5: " << NewNumber5 << endl;
+                cout << "Value of i: " << i << endl;
+                cout << "-----------------------" << endl;
 
-        std::cout << "-----------------------" << std::endl;
-        std::cout << "Value of NewNumber3: " << NewNumber3 << std::endl;
-        std::cout << "Value of NewNumber5: " << NewNumber5 << std::endl;
-        std::cout << "Value of i: " << i << std::endl;
-        std::cout << "-----------------------" << std::endl;
-        std::cout << "Value of OldNumber3 " << OldNumber3 << std::endl;
-        std::cout << "Value of OldNumber5 " << OldNumber5 << std::endl;
-        std::cout << "" << std::endl;
+                
+            }
+                
+        }
 
-        std::cout << "-----------------------" << std::endl;
+        if (OldNumber5 > 10)
+        {
+            cout << "WRONG" << endl;
+            return 0;
+        }
 
-        std::cout << "Value of the numbers added is: " << AddedNumber << std::endl;
-        std::cout << "-----------------------" << std::endl;
-        std::cout << "-----------------------" << std::endl;
-        std::cout << "" << std::endl;
+        if (OldNumber5 <= 10)
+        {
+            if (NewNumber5 <= 10)
+            {
+                GoodNumber5 = NewNumber5;
+                /*NewNumber3 = OldNumber3;*/
+                cout << "" << endl;
+                cout << "NewNumber5 if statement" << endl;
+                cout << "Goodnumber3: " << GoodNumber3 << endl;
+                cout << "Goodnumber5: " << GoodNumber5 << endl;
+                cout << "Newnumber3: " << NewNumber3 << endl;
+                cout << "NewNumber5: " << NewNumber5 << endl;
+                cout << "Value of i: " << i << endl;
+                cout << "-----------------------" << endl;
+
+                
+            }
+                       
         }
         
-
-        
-        
-        
-        
-        
-
-
-
+/*
         OldNumber3 = NewNumber3;
         OldNumber5 = NewNumber5;
-        
+ */
 
+        if (NewNumber3 > 10){}
+        else {
+            OldNumber3 = NewNumber3;
+        }
+        if (NewNumber5 > 10){}
+        else {
+            OldNumber5 = NewNumber5;
+        }
+        
         i++;
+        
     
     }
-    while (i <= 5);
-*/
+    
 
-    while (i < 5){
-
-        NewNumber3 = i * Number3;
-
-        std::cout << "Value of NewNumber3: " << NewNumber3 << std::endl;
-        std::cout << "Original value of OldNumber3: " << OldNumber3 << std::endl;
-
-        OldNumber3 = NewNumber3;
-        std::cout << "Value of OldNumber3: " << OldNumber3 << std::endl;
-        std::cout << "" << std::endl;
-
-
-        if (OldNumber3 > 10 > NewNumber3)
-        {
-            std::cout << "IT DID SOMETHING!" << std::endl;
-            return 0;
-        }
-        else
-        {
-            i++;
-        }
-                
-    }
 
     return 0;
     

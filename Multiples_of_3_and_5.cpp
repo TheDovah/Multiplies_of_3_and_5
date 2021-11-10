fa14b6ad3a71b3b8e105e322e364e112610e8eed
TheDovah <50626652+TheDovah@users.noreply.github.com>
2021-11-10 12:38:56 +0000

    Update Multiples_of_3_and_5.cpp

--- Multiples_of_3_and_5.cpp
+++ Multiples_of_3_and_5.cpp
@@ -21,39 +21,22 @@ int main ()
     int i = 1;
 
     int lim;
-    //cout << "" << GoodNumbers.max_size() << endl;
+    
     cout << "What is the limit you are after?" << endl;
     cout << "" << endl;
+    
     cin >> lim;
+    
     cout << "------------------" << endl;
     cout << "" << endl;
-    
-/*
-    cout << "Original value of Number3: " << Number3 << endl;
-    cout << "Original value of Number5: " << Number5 << endl;
-    cout << "Original value of i: " << i << endl;
-    cout << "-----------------------" << endl;
-*/
 
     while (i <= lim * 100)
     {   
         int NewNumber3 = i * Number3;
         int NewNumber5 = i * Number5;
 
-/*    
-        cout << "Value of Number3: " << Number3 << endl;
-        cout << "Value of Number5: " << Number5 << endl;
-        cout << "Value of NewNumber3: " << NewNumber3 << endl;
-        cout << "Value of OldNumber3: " << OldNumber3 << endl;
-        cout << "Value of NewNumber5: " << NewNumber5 << endl;
-        cout << "Value of OldNumber5: " << OldNumber5 << endl;
-        cout << "Value of i: " << i << endl;
-        cout << "-----------------------" << endl;
-*/
-
         if (OldNumber3 > lim || OldNumber5 > lim)
         {
-            //cout << "WRONG" << endl;
             return 0;
         }
 
@@ -62,109 +45,74 @@ int main ()
             if (NewNumber3 < lim)
             {
                 GoodNumbers.push_back(NewNumber3);
-/*
-                cout << "" << endl;
-                cout << "Newnumber3: " << NewNumber3 << endl;
-                cout << "NewNumber5: " << NewNumber5 << endl;
-                cout << "Value of i: " << i << endl;
-                cout << "-----------------------" << endl;
-*/
-            }
-                
-        }
-
-       
+            }                
+        }   
 
         if (OldNumber5 < lim)
         {
             if (NewNumber5 < lim)
             {
-                GoodNumbers.push_back(NewNumber5);
-/*
-                cout << "" << endl;
-                cout << "NewNumber5 if statement" << endl;
-                cout << "Newnumber3: " << NewNumber3 << endl;
-                cout << "NewNumber5: " << NewNumber5 << endl;
-                cout << "Value of i: " << i << endl;
-                cout << "-----------------------" << endl;
-*/
-                
-            }
-                       
+                GoodNumbers.push_back(NewNumber5);                
+            }           
         }
-        
 
-        
-        
-        
-
-
-        if (NewNumber3 > lim){break;}
-        else {
+        if (NewNumber3 > lim)
+        {
+            break;
+        }
+        else
+        {
             OldNumber3 = NewNumber3;
         }
-        if (NewNumber5 > lim){}
-        else {
+        if (NewNumber5 > lim)
+        {
+        
+        }
+        else
+        {
             OldNumber5 = NewNumber5;
         }
         
         cout << "This is round: " << i << endl;
 
         i++;
-        
-    
     }
-
 
     cout << "------------------" << endl;
     cout << "Sorting..." << endl;
     cout << "------------------" << endl;
-        sort(GoodNumbers.begin(), GoodNumbers.end());
+    
+    sort(GoodNumbers.begin(), GoodNumbers.end());
 
-        for (int b = 0; b < GoodNumbers.size(); b++)
+    for (int b = 0; b < GoodNumbers.size(); b++)
+    {
+        if (b > 0)
         {
-
-            if (b > 0)
+            if (GoodNumbers[b] == GoodNumbers[b-1])
             {
-
-                if (GoodNumbers[b] == GoodNumbers[b-1])
-                {
-                    //cout << "Something is being deleted" << endl;
-                    GoodNumbers.erase(GoodNumbers.begin() + b);
-                }
-                else
-                {
-                    //cout << "the last number is " << GoodNumbers[b-1] << endl;
-                }
-                
-                
+                GoodNumbers.erase(GoodNumbers.begin() + b);
             }
         }
-
-    //cout << "" << endl;
+    }
 
     for (int c = 0; c < GoodNumbers.size(); c++)
-        {
-            sum += GoodNumbers[c];
-            
-        }
-
+    {
+        sum += GoodNumbers[c]; 
+    }
 
     cout << "The multiples of 3 and 5 are: ";
+    
     for (int b = 0; b < GoodNumbers.size(); b++)
-        {
+    {
         cout << "" << GoodNumbers[b];
         cout << ", ";
-        }
+    }
+    
     cout << "" << endl;
     cout << "------------------------------" << endl;
     cout << "" << endl;
     cout << "The sum is: "<< sum << endl;
     cout << "" << endl;
     
-    
-    
     return 0;
-    
-    
 }
